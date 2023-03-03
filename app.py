24f8a59ee2cb024dbda87ea6a7fa4d412ad4bbe1
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-03-03 01:28:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,23 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 message_log = []
     
 def generate_response(message_log):
-    # Use OpenAI's ChatCompletion API to get the chatbot's response
+    # Use OpenAI's Completion API to get the chatbot's response
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
-        messages=message_log,   # The conversation history up to this point, as a list of dictionaries
-        # max_tokens=4096,        # The maximum number of tokens (words or subwords) in the generated response
-        # stop=None,              # The stopping sequence for the generated response, if any (not used here)
+        engine="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
+        prompt=message_log[-1]['content'],   # The most recent user message as the prompt
+        max_tokens=1024,        # The maximum number of tokens (words or subwords) in the generated response
+        n=1,        # The number of responses to generate
+        stop=None,              # The stopping sequence for the generated response, if any
         temperature=0.7,        # The "creativity" of the generated response (higher temperature = more creative)
     )
 
-    # Find the first response from the chatbot that has text in it (some responses may not have text)
-    for choice in response.choices:
-        if "text" in choice:
-            return choice.text
-
-    # If no response with text is found, return the first response's content (which may be empty)
-    return response.choices[0].message.content
+    # Extract the first response from the API response
+    return response.choices[0].text.strip()
 
 st.markdown("# OpenAI Chatbot - Escritor Fantasma")
 
-# Add a prompt for the user to start the conversation
-message_log.append({"role": "user", "content": "Hola, soy un escritor fantasma. Estoy aquí para ayudarte a escribir lo que necesites. ¿En qué puedo ayudarte hoy?"})
-output = generate_response(message_log)
-message_log.append({"role": "assistant", "content": output})
-st.markdown(f'''**AI:** {output}''')
+# Add a prompt for the chatbot to start the conversation
+message_log.append({"role": "assistant", "content": "Hola, soy un escritor fantasma. Estoy aquí para ayudarte a escribir lo que necesites. ¿En qué puedo ayudarte hoy?"})
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -49,12 +42,16 @@ if user_input:
     #store the output
     st.session_state['past'].append(user_input)
     st.session_state['generated'].append(output)
+    # Clear the text area for the next user input
+    st.session_state['input'] = ''
 
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        # message(st.session_state["generated"][i], key=str(i))
         st.markdown(f'''**AI:** {st.session_state["generated"][i]}''')
-        # message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
         st.markdown(f'''**You:** {st.session_state['past'][i]}''')
 
-
+# Initialize the text area for user input, or clear it if there is a new message
+if 'input' not in st.session_state:
+    st.session_state['input'] = ''
+user_input=st.text_area("You:", value=st.session_state['input'], key='input')
+st.session_state['input'] = user_input.strip()
